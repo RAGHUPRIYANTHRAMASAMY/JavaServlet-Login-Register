b3577fe3e574f6dbc3a5c095917fcbb92912c3e0
R <81795180+RAGHUPRIYANTHRAMASAMY@users.noreply.github.com>
2021-04-30 17:31:45 +0530

    Add files via upload

---
+++ src/main/java/com/zc/AddServlet.java
@@ -0,0 +1,30 @@
+package com.zc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet
+{
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		int j = Integer.parseInt(req.getParameter("num2"));
+		
+		int k = i+j;
+		
+//		PrintWriter out = res.getWriter();
+//		out.println("The result is: "+ k);
+//		res.getWriter().println("The result is: "+ k);
+		
+		req.setAttribute("k", k);
+		
+		RequestDispatcher rd = req.getRequestDispatcher("sq");
+		rd.forward(req, res);
+	}
+}
